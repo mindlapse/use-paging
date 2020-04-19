be61bf626621e1c772c684a5bc60ff16b5f1406a
Dave MacDonald <mindlapse@gmail.com>
2020-04-19 14:08:19 -0400

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,102 @@
-import * as React from 'react';
+import { useReducer, useEffect, Reducer } from "react";
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState<{
-    counter: number;
-  }>({
-    counter: 0
-  });
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++;
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval);
+const PER_PAGE = 2;
+
+const types = {
+  NEXT_PAGE: "NEXT_PAGE",
+  PREV_PAGE: "PREV_PAGE",
+  SET_ROWS: "SET_ROWS",
+};
+
+const initialState : UsePagingState = {
+  rows: [],
+  pageRows: [],
+  pageIndex: 0,
+  perPage: 30,
+};
+
+const init = (opts : UsePagingOptions) => () : UsePagingState => {
+  return {
+    ...initialState,
+    perPage: opts.perPage,
+  };
+};
+
+const setPage = (state:UsePagingState, rows:Array<any>, pageIndex:number) => {
+  const isNewRows = state.rows !== rows;
+  const isNewPageIndex = state.pageIndex !== pageIndex;
+  const numPages = Math.ceil(rows.length / state.perPage);
+
+  if (isNewPageIndex || isNewRows) {
+    if (pageIndex * state.perPage >= rows.length) {
+      pageIndex = numPages - 1;
+    }
+    if (pageIndex < 0) {
+      pageIndex = 0;
+    }
+
+    const start =
+      Math.max(0, Math.min(pageIndex, numPages - 1)) * state.perPage;
+
+    state = {
+      ...state,
+      rows,
+      pageIndex,
+      pageRows: rows.slice(start, start + state.perPage),
     };
+  }
+  return state;
+};
+
+const reducer : Reducer<UsePagingState, Action>= (state, action) => {
+  switch (action.type) {
+    case types.NEXT_PAGE:
+      state = setPage(state, state.rows, state.pageIndex + 1);
+      break;
+    case types.PREV_PAGE:
+      state = setPage(state, state.rows, state.pageIndex - 1);
+      break;
+    case types.SET_ROWS:
+      state = setPage(state, (action as SetRowsAction).rows, state.pageIndex);
+      break;
+    default:
+      console.error(action);
+      throw new Error("action.type not recognized");
+  }
+  return state;
+};
+
+
+const actions : UsePagingActions = {
+  nextPage: () => ({
+    type: types.NEXT_PAGE,
+  }),
+  prevPage: () => ({
+    type: types.PREV_PAGE,
+  }),
+  setRows: (rows:Array<any>) => ({
+    type: types.SET_ROWS,
+    rows,
+  }),
+};
+
+const usePaging = (rows:Array<any> = [], opts : UsePagingOptions = { perPage : PER_PAGE }) => {
+  const [state, dispatch] = useReducer(reducer, initialState, init(opts));
+
+  useEffect(() => {
+    setTimeout(() => dispatch(actions.setRows(rows)), 0);
   }, []);
 
-  return counter;
+  return {
+    pages: {
+      pageRows: state.pageRows,
+      pageIndex: state.pageIndex,
+      nextPage: () => dispatch(actions.nextPage()),
+      prevPage: () => dispatch(actions.prevPage()),
+      setRows: (rows:[]) => dispatch(actions.setRows(rows)),
+    },
+  };
 };
+
+export default usePaging;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="react-scripts" />
+
+interface UsePagingState {
+  rows: Array<any>;
+  pageRows: Array<any>;
+  pageIndex: number;
+  perPage: number;
+}
+
+interface UsePagingOptions {
+  perPage: number;
+}
+
+interface Action {
+  type: string;
+}
+type SetRowsAction = Action & { rows: Array<any> };
+
+interface UsePagingActions {
+  nextPage: () => Action;
+  prevPage: () => Action;
+  setRows: (rows: Array<any>) => SetRowsAction;
+}
